78e1d770c9c6188b13a48a3fae584997f3b10334
Mohan, Padmanabhan <Padmanabhan.Mohan@in.pega.com>
2016-12-11 11:16:24 +0530

    Base code of BB.com on Sun 11, 2016

---
+++ js/dealer.js
@@ -0,0 +1,250 @@
+var dealerData = {
+  'Eurocycles': {
+    'latitude': '-33.810497',
+    'longitude': '151.148186'
+  },
+  'Treadly Bike shop': {
+    'latitude': '-34.922962',
+    'longitude': '138.609272'
+  },
+  'International Cycles, ': {
+    'latitude': '-34.911737',
+    'longitude': '138.626333'
+  },
+  'PS Bikes': {
+    'latitude': '-37.818231',
+    'longitude': '144.896725'
+  },
+  'Brisbane': {
+    'latitude': '-27.464345',
+    'longitude': '153.049241'
+  },
+  'Mountainbiker Klagenfurt': {
+    'latitude': '46.623627',
+    'longitude': '14.310749'
+  },
+  'GerRad': {
+    'latitude': '48.300266',
+    'longitude': '14.284316'
+  },
+  'Heavy Pedals': {
+    'latitude': '48.187509',
+    'longitude': '16.346922'
+  },
+  'Au Guidon Vert': {
+    'latitude': '50.83657',
+    'longitude': '4.380102'
+  },
+  'Bakfiestinfo.be - (Mobo Sevice)': {
+    'latitude': '51.249753',
+    'longitude': '4.497706'
+  },
+  'CYCLART': {
+    'latitude': '51.090315',
+    'longitude': '3.715077'
+  },
+  'Het Fietslab': {
+    'latitude': '50.884365',
+    'longitude': '4.719001'
+  },
+  'Allo Velo Inc.': {
+    'latitude': '45.469669',
+    'longitude': '-73.592918'
+  },
+  'Butchers & Bicycles': {
+    'latitude': '55.669071',
+    'longitude': '12.560568'
+  },
+  'Fully Charged': {
+    'latitude': '51.502451',
+    'longitude': '-0.082464'
+  },
+  'Kultaiset Pojat Oy': {
+    'latitude': '60.176894',
+    'longitude': '24.724853'
+  },
+  'Lyon Cycle Chic': {
+    'latitude': '45.730425',
+    'longitude': '4.839938'
+  },
+  'Les Cyclistes Branches': {
+    'latitude': '48.848428',
+    'longitude': '2.275097'
+  },
+  'LULU CYCLES': {
+    'latitude': '48.081864',
+    'longitude': '7.352921'
+  },
+  'Hobby Cycles': {
+    'latitude': '48.40412',
+    'longitude': '-4.480609'
+  },
+  'Bike Boutique': {
+    'latitude': '50.10967',
+    'longitude': '8.67084'
+  },
+  'Koech2rad': {
+    'latitude': '53.698963',
+    'longitude': '10.77106'
+  },
+  'MEIßNER RAEDER': {
+    'latitude': '51.06752',
+    'longitude': '13.74998'
+  },
+  'Two Wheels Good': {
+    'latitude': '53.57475',
+    'longitude': '9.9779'
+  },
+  'Fahrradladen Rückenwind': {
+    'latitude': '54.31669',
+    'longitude': '10.06669'
+  },
+  'Punta Velo GmbH': {
+    'latitude': '51.661755',
+    'longitude': '6.963257'
+  },
+  'WingWheels': {
+    'latitude': '52.53461',
+    'longitude': '13.40609'
+  },
+  'OrangeBikeConcept - Hannover': {
+    'latitude': '52.39',
+    'longitude': '9.735'
+  },
+  'Greenmobility Nord': {
+    'latitude': '53.091524',
+    'longitude': '8.790941'
+  },
+  'Hyggelig Bikes': {
+    'latitude': '48.76473',
+    'longitude': '9.17105'
+  },
+  'Velo Company': {
+    'latitude': '48.15696',
+    'longitude': '11.56247'
+  },
+  'Greenaer': {
+    'latitude': '53.343927',
+    'longitude': '-6.248247'
+  },
+  'Skootz': {
+    'latitude': '52.737727',
+    'longitude': '6.494388'
+  },
+  'DrBeyk': {
+    'latitude': '52.342729',
+    'longitude': '4.901569'
+  },
+  'JUIZZ - Den Haag': {
+    'latitude': '52.085343',
+    'longitude': '4.338432'
+  },
+  'JUIZZ - Amsterdam': {
+    'latitude': '52.361294',
+    'longitude': '4.870435'
+  },
+  'JUIZZ - Utrecht': {
+    'latitude': '52.095456',
+    'longitude': '5.130973'
+  },
+  'JUIZZ - Rotterdam': {
+    'latitude': '51.923316',
+    'longitude': '4.489719'
+  },
+  'Vive le velo': {
+    'latitude': '52.149591',
+    'longitude': '5.384062'
+  },
+  'Bikes and Barbers': {
+    'latitude': '-36.86722',
+    'longitude': '174.777569'
+  },
+  'Onya bikes Waiheke': {
+    'latitude': '-36.782519',
+    'longitude': '175.00957'
+  },
+  'Avanti+': {
+    'latitude': '-41.513947',
+    'longitude': '173.955851'
+  },
+  'EVO / G.  Nordby': {
+    'latitude': '58.146451',
+    'longitude': '7.990215'
+  },
+  'EVO Elsykler Skøyen': {
+    'latitude': '59.916798',
+    'longitude': '10.664616'
+  },
+  'EVO / Sykkelcenteret': {
+    'latitude': '58.999259',
+    'longitude': '5.619085'
+  },
+  'EVO Elsykler Tromsø': {
+    'latitude': '69.623556',
+    'longitude': '18.51826'
+  },
+  'Gaarder Sykkelsport': {
+    'latitude': '59.220537',
+    'longitude': '10.934701'
+  },
+  'Sykkelsport AS': {
+    'latitude': '58.465829',
+    'longitude': '8.760764'
+  },
+  'Waterfall': {
+    'latitude': '52.129871',
+    'longitude': '21.078331'
+  },
+  'Electriccycle Company': {
+    'latitude': '55.976138',
+    'longitude': '3.215744'
+  },
+  'Yes We Bike': {
+    'latitude': '39.502945',
+    'longitude': '2.471542'
+  },
+  'Ekern & Gamla Enskede Ladcyklar': {
+    'latitude': '59.274388',
+    'longitude': '18.133849'
+  },
+  'ELCYKELBUTIKEN I GÖTEBORG': {
+    'latitude': '57.69193',
+    'longitude': '11.95624'
+  },
+  'Velobloom': {
+    'latitude': '58.756695',
+    'longitude': '17.020444'
+  },
+  'LF Produkter AB': {
+    'latitude': '',
+    'longitude': ''
+  },
+  'OBST & GEMÜSE jam GmbH': {
+    'latitude': '47.562772',
+    'longitude': '7.591588'
+  },
+  'Velociped': {
+    'latitude': '47.033651',
+    'longitude': '8.278101'
+  },
+  'Version Originale Cycles Sàrl': {
+    'latitude': '46.788094',
+    'longitude': '6.630968'
+  },
+  'Splendid Cycles': {
+    'latitude': '45.504333',
+    'longitude': '-122.66168'
+  },
+  'Peoples Bicycle': {
+    'latitude': '41.508739',
+    'longitude': '-73.96545'
+  },
+  'Vie Bikes': {
+    'latitude': '37.773799',
+    'longitude': '-122.404935'
+  },
+  'location': {
+    'latitude': '',
+    'longitude': ''
+  }
+};
---
+++ js/gmap copy.js
@@ -0,0 +1,297 @@
+// function initMap(){
+window.initMap = function(){
+
+    var mapDiv = document.getElementById('map');
+    var myLatLng = [{lat: 57.554365, lng: 18.480604}, {lat: 51.503454, lng: 15.480604}, {lat: 53.554365, lng: 16.480604}];
+    // var myLatLng = {lat: 57.554365, lng: 18.480604};
+
+    var map = new google.maps.Map(mapDiv, {
+        center: {lat: 52.605842, lng: 10.222253},
+        scrollwheel: true,
+        zoom: 4
+    });
+    var markers = [];
+    // var iconBase = 'images';
+    var iconBase = '';
+    for(i=0 ; i<myLatLng.length; i++){
+            var dynamicLatLng = myLatLng[i]
+            var marker = new google.maps.Marker({
+              position: dynamicLatLng,
+              map: map,
+              // icon: iconBase + 'marker.png',
+              icon: iconBase + 'mapmarker.png'
+            });
+            marker.addListener('click', function() {
+            $(".map_infowindow").hide();
+            $(".map_infowindow").fadeIn(500);
+            });
+            markers.push(marker);
+    }
+    var markerCluster = new MarkerClusterer(map, markers);
+    markerCluster.gridSize = 10;
+    markerCluster.maxZoom = 2;
+    markerCluster.setGridSize(20);
+    markerCluster.setMaxZoom(3);
+    // markerCluster.fitMapToMarkers(true);
+    console.log(markerCluster.getGridSize());
+    map.set('styles',[
+        {
+            "featureType": "all",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "saturation": 36
+                },
+                {
+                    "color": "#333333"
+                },
+                {
+                    "lightness": 40
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#ffffff"
+                },
+                {
+                    "lightness": 16
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#fefefe"
+                },
+                {
+                    "lightness": 20
+                }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "color": "#fefefe"
+                },
+                {
+                    "lightness": 17
+                },
+                {
+                    "weight": 1.2
+                }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "simplified"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative.country",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "hue": "#ff0000"
+                },
+                {
+                    "lightness": "-30"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative.locality",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#858585"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#dedede"
+                },
+                {
+                    "lightness": 20
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#f5f5f5"
+                },
+                {
+                    "lightness": 21
+                }
+            ]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#dedede"
+                },
+                {
+                    "lightness": 21
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                },
+                {
+                    "lightness": 17
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                },
+                {
+                    "lightness": 29
+                },
+                {
+                    "weight": 0.2
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                },
+                {
+                    "lightness": 18
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                },
+                {
+                    "lightness": 16
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#f2f2f2"
+                },
+                {
+                    "lightness": 19
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#e9e9e9"
+                },
+                {
+                    "lightness": 17
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "lightness": "-30"
+                },
+                {
+                    "weight": "3.80"
+                },
+                {
+                    "saturation": "0"
+                },
+                // {
+                //     "color":"#00264d"
+                // }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "visibility": "on"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        }
+]);
+
+
+/*var marker = new google.maps.Marker({
+  position: myLatLng,
+  map: map,
+  icon: iconBase + 'info-i_maps.png'
+});*/
+
+
+      }
